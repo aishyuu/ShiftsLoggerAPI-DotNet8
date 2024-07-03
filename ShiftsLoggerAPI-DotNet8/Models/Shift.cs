5728bd1bf38b391fa3db9d5ac003ecf432724048
aishyuu <ivanmatias1331@gmail.com>
2024-07-03 16:35:17 -0700

    Added Shift model

---
+++ ShiftsLoggerAPI-DotNet8/Models/Shift.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftsLoggerAPI_DotNet8.Models
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+        public string EmployeeFirstName { get; set; } = string.Empty;
+        public string EmployeeLastName { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
