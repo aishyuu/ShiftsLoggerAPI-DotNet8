10bf9df70b5f25a7383d8599a1acfdac78ef7d8d
aishyuu <ivanmatias1331@gmail.com>
2024-07-03 17:47:06 -0700

    Added Data Context

---
+++ ShiftsLoggerAPI-DotNet8/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerAPI_DotNet8.Models;
+
+namespace ShiftsLoggerAPI_DotNet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Shift> Shifts { get; set; }
+    }
+}
