ff145d50429bdb399294f0bc90c9aea98d241e5e
aishyuu <ivanmatias1331@gmail.com>
2024-07-03 21:49:22 -0700

    Added "Get shift", "Add Shift", and "Update Shift" functions

--- ShiftsLoggerAPI-DotNet8/Controllers/ShiftController.cs
+++ ShiftsLoggerAPI-DotNet8/Controllers/ShiftController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerAPI_DotNet8.Data;
 using ShiftsLoggerAPI_DotNet8.Models;
 
 namespace ShiftsLoggerAPI_DotNet8.Controllers
@@ -8,22 +10,57 @@ namespace ShiftsLoggerAPI_DotNet8.Controllers
     [ApiController]
     public class ShiftController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ShiftController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<Shift>>> GetAllShifts()
         {
-            var shifts = new List<Shift>
-            {
-                new Shift
-                {
-                    Id = 1,
-                    EmployeeFirstName = "John",
-                    EmployeeLastName = "Smith",
-                    StartTime = new DateTime(2024, 7, 1, 9, 0, 0),
-                    EndTime = new DateTime(2024, 7, 1, 16, 0, 0)
-                }
-            };
+            var shifts = await _context.Shifts.ToListAsync();
 
             return Ok(shifts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Shift>>> GetShift(int id)
+        {
+            var shift = await _context.Shifts.FindAsync(id);
+            if(shift is null)
+            {
+                return BadRequest("Shift not found!");
+            }
+
+            return Ok(shift);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Shift>>> AddShift(Shift newShift)
+        {
+            _context.Shifts.Add(newShift);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Shifts.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Shift>>> UpdateShift(Shift updatedShift)
+        {
+            var shift = await _context.Shifts.FindAsync(updatedShift.Id);
+            if (shift is null)
+            {
+                return BadRequest("Shift has not been found");
+            }
+
+            shift.EmployeeFirstName = updatedShift.EmployeeFirstName;
+            shift.EmployeeLastName = updatedShift.EmployeeLastName;
+            shift.StartTime = updatedShift.StartTime;
+            shift.EndTime = updatedShift.EndTime;
+
+            return Ok(await _context.Shifts.ToListAsync());
         }
     }
 }
